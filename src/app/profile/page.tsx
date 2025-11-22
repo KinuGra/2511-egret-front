67133f9c6348b65ba7c1a96ca32d51d7ff0cc21b
air_kinu <techgit1024@gmail.com>
2025-11-23 03:20:39 +0900

    add: SnippetCardにスコアバッジを表示

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,78 +12,79 @@ import { PlanetaryComparison } from "@/app/profile/components/ComparePlanet";
 import { getSnippet } from "@/lib/firestore/getSnippet";
 
 export default function ProfileScreen() {
-	const [snippets, setSnippets] = useState<any[]>([]);
-	const [isFormVisible, setIsFormVisible] = useState(false);
-	const [notification, setNotification] = useState<{
-		title: string;
-		content: string;
-		snippetScore: number;
-	} | null>(null);
+  const [snippets, setSnippets] = useState<any[]>([]);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [notification, setNotification] = useState<{
+    title: string;
+    content: string;
+    snippetScore: number;
+  } | null>(null);
 
-	// スニペットを読み込む
-	async function loadSnippet() {
-		const docs = await getSnippet();
-		setSnippets(docs);
-	}
-	const { sendMessage } = useWebSocket({
-		url: "wss://etuqhxwxk1.execute-api.ap-northeast-1.amazonaws.com/Prod/",
-		onMessage: (data) => {
-			setNotification(data);
-			loadSnippet();
-		},
-	});
-	const scores: [number, number, number] = [100000, 1000, 10030];
+  // スニペットを読み込む
+  async function loadSnippet() {
+    const docs = await getSnippet();
+    setSnippets(docs);
+  }
+  const { sendMessage } = useWebSocket({
+    url: "wss://etuqhxwxk1.execute-api.ap-northeast-1.amazonaws.com/Prod/",
+    onMessage: (data) => {
+      setNotification(data);
+      loadSnippet();
+    },
+  });
+  const scores: [number, number, number] = [100000, 1000, 10030];
 
-	useEffect(() => {
-		loadSnippet();
-	}, []);
+  useEffect(() => {
+    loadSnippet();
+  }, []);
 
-	return (
-		<>
-			{notification && (
-				<Notification
-					title={notification.title}
-					content={notification.content}
-					snippetScore={notification.snippetScore}
-					onClose={() => setNotification(null)}
-				/>
-			)}
-			<div style={{ backgroundColor: "#efefef" }}>
-				<PlanetaryComparison score={scores} />
-				<EngineerComparisonProgress comparison={topEngineerData} />
-				<EngineerComparisonProgress comparison={surroundingEngineerData} />
-				{snippets.length > 0
-					? snippets.map((snippet, index) => (
-							<div key={snippet.id}>
-								<SnippetCard
-									id={snippet.id}
-									title={snippet.data.title}
-									content={snippet.data.content}
-								/>
-							</div>
-						))
-					: "No data loaded."}
-			</div>
-			<EditButton onClick={() => setIsFormVisible(true)} />
-			<EditForm
-				isOpen={isFormVisible}
-				onClose={() => setIsFormVisible(false)}
-				sendMessage={sendMessage}
-				loadSnippet={loadSnippet}
-			/>
-		</>
-	);
+  return (
+    <>
+      {notification && (
+        <Notification
+          title={notification.title}
+          content={notification.content}
+          snippetScore={notification.snippetScore}
+          onClose={() => setNotification(null)}
+        />
+      )}
+      <div style={{ backgroundColor: "#efefef" }}>
+        <PlanetaryComparison score={scores} />
+        <EngineerComparisonProgress comparison={topEngineerData} />
+        <EngineerComparisonProgress comparison={surroundingEngineerData} />
+        {snippets.length > 0
+          ? snippets.map((snippet, index) => (
+              <div key={snippet.id}>
+                <SnippetCard
+                  id={snippet.id}
+                  title={snippet.data.title}
+                  content={snippet.data.content}
+                  scoreFromAWS={snippet.data.snippetScore}
+                />
+              </div>
+            ))
+          : "No data loaded."}
+      </div>
+      <EditButton onClick={() => setIsFormVisible(true)} />
+      <EditForm
+        isOpen={isFormVisible}
+        onClose={() => setIsFormVisible(false)}
+        sendMessage={sendMessage}
+        loadSnippet={loadSnippet}
+      />
+    </>
+  );
 }
 
 const topEngineerData = {
-	title: "VS 活躍する学生",
-	myScore: 650,
-	targetScore: 800,
-	colorCode: "red",
+  title: "VS 活躍する学生",
+  myScore: 650,
+  targetScore: 800,
+  colorCode: "red",
 };
 const surroundingEngineerData = {
-	title: "VS 他プレイヤー",
-	myScore: 500,
-	targetScore: 550,
-	colorCode: "orange",
+  title: "VS 他プレイヤー",
+  myScore: 500,
+  targetScore: 550,
+  colorCode: "orange",
 };
